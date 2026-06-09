105af50a73013224e4027ea312533d13b6dbfa34
Lucas Klein <l.ucaskleindearaujo@hotmail.com>
2026-06-09 12:06:47 -0300

    refactor: translate output strings to English

--- src/main/java/org/example/SMSNotification.java
+++ src/main/java/org/example/SMSNotification.java
@@ -3,6 +3,6 @@ package org.example;
 public class SMSNotification implements Notification {
     @Override
     public void send(String message) {
-        System.out.println("Enviando SMS: " + message);
+        System.out.println("[SMS] Sending: " + message);
     }
 }
